ac3b5d2b9a5c042a68e48262634c2cd7189fa7b1
Harjot Singh <harjot.singh0712@gmail.com>
2022-03-05 11:57:40 -0800

    switched to GridBagLayout and moved elements

--- BaseConverter.java
+++ BaseConverter.java
@@ -21,18 +21,31 @@ public class BaseConverter implements ActionListener{
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
-        panel.setLayout(new GridLayout(0, 5, 30, 30));
-        panel.add(label1);
-        panel.add(label3);
-        panel.add(textField1);
-        panel.add(label5);
-        panel.add(textField2);
-        panel.add(label2);
-        panel.add(label4);
-        panel.add(textField3);
-        panel.add(label6);
-        panel.add(textField4);
-        panel.add(button1);
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+	    c.gridy = 0;
+        c.ipadx = 100;
+        c.ipady = 20;
+        // c.weightx = 1;
+        // c.weighty = 1;
+        c.insets = new Insets(2,2,2,2);
+
+        panel.add(label1, c);
+        c.gridx = GridBagConstraints.RELATIVE;
+        panel.add(textField1, c);
+        panel.add(label3, c);
+        panel.add(textField2, c);
+        panel.add(label5, c);
+	    c.gridy = 2;
+        panel.add(label2, c);
+        c.gridx = GridBagConstraints.RELATIVE;
+        panel.add(textField3, c);
+        panel.add(label4, c);
+        panel.add(textField4, c);
+        panel.add(label6, c);
+	    c.gridy = 3;
+        panel.add(button1, c);
 
 
         frame.add(panel, BorderLayout.CENTER);
@@ -44,7 +57,6 @@ public class BaseConverter implements ActionListener{
 
     public void actionPerformed(ActionEvent e)
     {
-        // if the state combobox is changed
             System.out.println("bruh");
     }
 
