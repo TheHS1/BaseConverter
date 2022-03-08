0d9b8ce9da80c12861744c103dd580183ad086c4
Ausxh <ausxh66@gmail.com>
2022-03-08 13:36:43 -0800

    hopefully finished NumberBase.add()
    have not tested, need to finish digit.add()

--- NumberBase.java
+++ NumberBase.java
@@ -23,23 +23,30 @@ public class NumberBase {
 	
 	public NumberBase add(NumberBase a, base b) {
 		if (this instanceOf Digit && a instanceOf Digit) {
-			return this.add(a);
+			return this.add(a).digits[1];
 		}
 		else {
 			this.length() = len1;
 			a.length() = len2;
+			int max;
+			int longer = 0;
 			
-			if (len1 >= len2) {int iter = len2; } //take smaller length
-			else { int max = len1; }
+			if (len1 >= len2) { max = len2; longer = 1; } //take smaller length, keep track of which is longer
+			else { max = len1; longer = 2; }
 			
 			String result = "";
-			NumberBase tempsum;
-			for (int i = 0; i < max; i--) {
-				tempsum = this[].add(a);
-				result = this.add(a).digits[] + result; //requires 
-			}
+			NumberBase tempsum, carry; //tempsum used for adding digits, carry used for carryover
+			
+			for (int i = 1; i <= max; i--) { //add digits starting from the right
+				tempsum = this.digits[len1-i].add(a.digits[len2-i]).add(carry);
+				result = this.add(a).digits[1].value + result; //adds the current sum to the beginning of the current result
+				carry = result.digits[0]; //set carryover
+			} //finish
+			if (longer == 1) { result = this.value.substring(0, len1-len2-1) + this.digits[len1-len2-1].add(carry) + result; }
+			else if (longer == 2) { result = a.value.substring(0, len2-len1-1) + a.digits[len2-len1-1].add(carry) + result; }
+			
+			return NumberBase(result);
 		}
-		return Number(result);
 	}
 
 }
