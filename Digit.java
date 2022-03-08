9d0979155aa4f4ee4c2fba6af2a7a8a20c091c90
Ausxh <ausxh66@gmail.com>
2022-03-08 13:44:49 -0800

    Update Digit.java

--- Digit.java
+++ Digit.java
@@ -1,4 +1,4 @@
-public class Digit extends Number {
+public class Digit extends NumberBase {
 	private static String[] values = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
 	                                  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
 					  "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
@@ -8,19 +8,16 @@ public class Digit extends Number {
 	                                  "Y", "Z", ":", "/"};
 	
 	public Digit(String value, int base) {
-		if (value.length() = 1) {
-			super(value, base);
-		}
+		super(value, base);
 	}
 	
-	@Override
-	public Number add(Digit a, base b) {
+	public NumberBase add(Digit a, base b) {
 		int temp = this.value() + a.value();
 		if (temp >= b) { //handles carryover (if 8 + 9 returns 17)
-			return new Number(values[1] + values[temp % b], b);
+			return new NumberBase(values[1] + values[temp % b], b);
 		}
 		else { 
-			return new Digit(temp, b);
+			return new NumberBase(0 + values[temp], b);
 		}			
 	}
 	
