60b5e73a29ba1e01ddc0d20a475195234db1afa3
Harjot Singh <harjot.singh0712@gmail.com>
2022-03-16 18:58:02 -0700

    [refactoring]moved components into Components.java

--- BaseConverter.java
+++ BaseConverter.java
@@ -9,7 +9,6 @@ public class BaseConverter implements ActionListener,ItemListener {
     private JComboBox<String> operations;
     private JPanel elemList, content;
     private JFrame frame;
-
     private JButton calculate, add, sub;
 
     public BaseConverter() {
@@ -50,36 +49,13 @@ public class BaseConverter implements ActionListener,ItemListener {
         content = new JPanel(new CardLayout());
         main.add(content, c);
 
-        JPanel convert = new JPanel();
-        convert.add(new JLabel("test"));
-        content.add(convert, "Convert");
-
-        elemList = new JPanel();
-        elemList.setLayout(new GridLayout(0, 1));
-        content.add(elemList, "Operate");
-
-        c.insets = new Insets(0, 0, 0, 0);
-        JPanel controls = new JPanel(new GridBagLayout());
-        GridBagConstraints cc = new GridBagConstraints();
-        cc.fill = java.awt.GridBagConstraints.BOTH;
-        cc.gridy = 1;
-        cc.ipadx = 40;
-        cc.ipady = 10;
-        controls.add(sub, cc);
-        cc.gridy = 0;
-        cc.gridx = GridBagConstraints.RELATIVE;;
-        controls.add(add, cc);
-        cc.gridx = 1;
-        cc.gridheight = 2;
-        controls.add(calculate, cc);
-        elemList.add(controls);
+        content.add(new ConvertView(), "Convert");
+        content.add(new OperationsView(add, sub, calculate), "Operate");
 
         c.gridy = 4;
         total = new JLabel();
         main.add(total, c);
 
-        addRow();
-        addRow();
         frame.setContentPane(main);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
@@ -88,53 +64,15 @@ public class BaseConverter implements ActionListener,ItemListener {
 
     }
 
-    public void addRow() {
-        if(TermRow.getCount() <= 4) {
-            if(elemList.getComponentCount() > 1) {
-                elemList.add(new Operations(), elemList.getComponentCount()-1);
-            }
-            elemList.add(new TermRow(), elemList.getComponentCount()-1);
-        }
-    }
-
-    public void totalValues() {
-        TermRow firstTerm = (TermRow) elemList.getComponent(0);
-        int sum = firstTerm.getValue();
-        for (int i = 1; i < (elemList.getComponentCount()) / 2 + 2; i+=2) {
-            Operations o = (Operations) elemList.getComponent(i);
-            TermRow secondTerm = (TermRow) elemList.getComponent(i+1);
-            if (o.getOperations().getSelectedItem() == "add") {
-                sum += secondTerm.getValue();
-                total.setText(sum + "");
-            }
-            else if (o.getOperations().getSelectedItem() == "multiply") {
-                sum *= secondTerm.getValue();
-                total.setText(sum + "");
-            }
-        }
-
-    }
-
-    public void removeRow() {
-        if(TermRow.getCount() >= 4) {
-            try {
-                elemList.remove(elemList.getComponentCount()-3);
-                elemList.remove(elemList.getComponentCount()-3);
-                TermRow.decCount();
-            } catch(Exception e) {
-                
-            }
-        }
-    }
-    
     @Override
     public void actionPerformed(ActionEvent e) {
+        OperationsView opView = (OperationsView) content.getComponent(1);
         if (e.getSource() == add) {
-            addRow();
+            opView.addRow();
         } else if (e.getSource() == sub) {
-            removeRow();
+            opView.removeRow();
         } else if (e.getSource() == calculate) {
-            totalValues();
+            this.total.setText(opView.totalValues() + "");
         }
         frame.pack();
     } 
--- Components.java
+++ Components.java
@@ -18,14 +18,13 @@ class TermRow extends JPanel{
         this.setLayout(new GridBagLayout());
 
         GridBagConstraints c = new GridBagConstraints();
-        c.gridx = GridBagConstraints.RELATIVE;;
+        c.gridx = GridBagConstraints.RELATIVE;
         c.fill = java.awt.GridBagConstraints.HORIZONTAL;
         c.gridy = 0;
         c.ipadx = 100;
         c.ipady = 20;
         c.weightx = 0;
         c.weighty = 1;
-
 
         Insets defaultMargin = new Insets(0,0,0,0);
         c.insets = defaultMargin;
@@ -35,7 +34,6 @@ class TermRow extends JPanel{
         this.add(new JLabel("base"), c);
         this.add(value, c);
         this.add(new JLabel("value"), c);
-        this.setBorder(BorderFactory.createLineBorder(Color.black));
         count++;
 
     }
@@ -57,15 +55,114 @@ class TermRow extends JPanel{
     }
 }
 
+class ConvertView extends JPanel {
+    private GridBagConstraints c; 
+    private JTextField base, value;
+
+    public ConvertView() {
+        this.setLayout(new GridBagLayout());
+        base = new JTextField();
+        value = new JTextField();
+        c = new GridBagConstraints();
+        
+        c.gridx = GridBagConstraints.RELATIVE;;
+        c.gridy = 0;
+        c.ipadx = 100;
+        c.ipady = 20;
+        c.weightx = 0;
+        c.weighty = 1;
+        c.fill = java.awt.GridBagConstraints.HORIZONTAL;
+
+        this.add(new JLabel("base"), c);
+        this.add(base, c);
+
+        c.gridy = 1;
+        this.add(new JLabel("value"), c);
+        this.add(value, c);
+
+    }
+}
+
+class OperationsView extends JPanel {
+
+    public OperationsView(JButton add, JButton sub, JButton calculate) {
+        this.setLayout(new GridLayout(0, 1));
+        this.add(new Controls(add, sub, calculate));
+        // todo add another enclosing panel for rows to separate from calculations
+        this.addRow();
+        this.addRow();
+    }
+
+    public void addRow() {
+        if(TermRow.getCount() <= 4) {
+            if(this.getComponentCount() > 1) {
+                this.add(new Operations(), this.getComponentCount()-1);
+            }
+            this.add(new TermRow(), this.getComponentCount()-1);
+        }
+    }
+
+    public void removeRow() {
+        if(TermRow.getCount() >= 4) {
+            try {
+                this.remove(this.getComponentCount()-2);
+                this.remove(this.getComponentCount()-2);
+                TermRow.decCount();
+            } catch(Exception e) {
+                
+            }
+        }
+    }
+
+    public int totalValues() {
+        TermRow firstTerm = (TermRow) this.getComponent(0);
+        int sum = firstTerm.getValue();
+        for (int i = 1; i < (this.getComponentCount()) / 2 + 2; i+=2) {
+            Operations o = (Operations) this.getComponent(i);
+            TermRow secondTerm = (TermRow) this.getComponent(i+1);
+            if (o.getOperations().getSelectedItem() == "add") {
+                sum += secondTerm.getValue();
+            }
+            else if (o.getOperations().getSelectedItem() == "multiply") {
+                sum *= secondTerm.getValue();
+            }
+        }
+        return sum;
+
+    }
+}
+
+class Controls extends JPanel {
+    private GridBagConstraints c;
+
+    public Controls(JButton add, JButton sub, JButton calculate) {
+
+        this.setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
+        c.fill = java.awt.GridBagConstraints.BOTH;
+        c.gridy = 1;
+        c.ipadx = 40;
+        c.ipady = 10;
+        this.add(sub, c);
+        c.gridy = 0;
+        c.gridx = GridBagConstraints.RELATIVE;
+        this.add(add, c);
+        c.gridx = 1;
+        c.gridheight = 2;
+        this.add(calculate, c); // TODO add button
+    }
+}
+
 class Operations extends JPanel {
     private final String[] operators = {"add", "multiply"};
     private JComboBox<String> operations;
+    private GridBagConstraints c;
 
     public Operations() {
         operations = new JComboBox<>(operators);
         this.setLayout(new GridBagLayout());
         this.add(operations);
-        GridBagConstraints c = new GridBagConstraints();
+        c = new GridBagConstraints();
         c.gridx = GridBagConstraints.RELATIVE;;
         c.gridy = 0;
         c.ipadx = 300;
